4c6149c252694486f93ad622909a8fffa9c01bc1
Jesus Liang <jesusliang96@gmail.com>
2022-10-29 22:14:42 +0200

    fixed no id defined in interface

--- src/database/types/user.type.ts
+++ src/database/types/user.type.ts
@@ -1,4 +1,7 @@
-export interface IUser {
+import { InferAttributes, InferCreationAttributes, Model } from 'sequelize';
+
+export interface IUser extends Model<InferAttributes<IUser>, InferCreationAttributes<IUser>> {
+  id?: string;
   email: string;
   password: string;
   firstName: string;
