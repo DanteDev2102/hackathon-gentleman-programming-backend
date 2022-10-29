22f70fb349042285127be7730925d6106a5d918e
Jesus Liang <jesusliang96@gmail.com>
2022-10-29 22:15:35 +0200

    added login route and getUser route for debugging

--- src/routes/user/user.routes.ts
+++ src/routes/user/user.routes.ts
@@ -1,11 +1,13 @@
-import { RegisterUserSchema } from '../../schemas/user/user.schema';
+import { LoginUserSchema, RegisterUserSchema } from '../../schemas/user/user.schema';
 import { validateSchema } from '../../middlewares';
 import { IRouter, Router } from 'express';
-import { getUsers, registerUser } from '../../controllers';
+import { getLoggedUser, getUsers, registerUser, userLogin } from '../../controllers';
 
 const userRoutes: IRouter = Router();
 
 userRoutes.get('/', getUsers);
 userRoutes.post('/register', validateSchema(RegisterUserSchema), registerUser);
+userRoutes.post('/login', validateSchema(LoginUserSchema), userLogin);
+userRoutes.get('/getUser', getLoggedUser);
 
 export default userRoutes;
