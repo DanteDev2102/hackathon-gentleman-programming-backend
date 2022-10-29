63d991baa86138f663cb9c3d8f9a19ed0accc995
Jesus Liang <jesusliang96@gmail.com>
2022-10-29 22:14:00 +0200

    added user login logic

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -1,11 +1,13 @@
 import { AppError } from './../../utils/appError';
-import { CreateUserParams } from '@/routes/user/user.routes.type';
+import { CreateUserParams, UserLoginParams } from '../../routes';
 import { hash } from 'bcrypt';
 import { Request, Response } from 'express';
 import { UserSchema } from '../../database';
-import { EmailAlreadyExistsError } from './user.controller.errors';
+import { EmailAlreadyExistsError, InvalidUserData } from './user.controller.errors';
+import { compare } from 'bcryptjs';
+import { RequestWithSession } from '../../types/sessions';
 
-export const registerUser = async (req: Request, res: Response) => {
+export const registerUser = async (req: RequestWithSession, res: Response) => {
   try {
     const { email, firstName, lastName, password }: CreateUserParams = req.body;
     const emailAlreadyExists = await UserSchema.findOne({
@@ -22,6 +24,7 @@ export const registerUser = async (req: Request, res: Response) => {
       lastName,
       password: await hash(password, 10)
     });
+    req.session.user = User;
     res.json(User);
   } catch (error) {
     if (error instanceof AppError) {
@@ -38,5 +41,39 @@ export const getUsers = async (_req: Request, res: Response) => {
     res.json(Users);
   } catch (error) {
     res.send(error);
+  }
+};
+
+export const getLoggedUser = async (req: RequestWithSession, res: Response) => {
+  try {
+    console.log(req.session.user?.email);
+    res.send(req.session.user);
+  } catch (error) {
+    res.send(error);
+  }
+};
+
+export const userLogin = async (req: RequestWithSession, res: Response) => {
+  try {
+    const loginData: UserLoginParams = req.body;
+    const User = await UserSchema.findOne({
+      where: {
+        email: loginData.email
+      }
+    });
+    if (!User) throw new InvalidUserData();
+    const isPasswordValid = await compare(loginData.password, User.password);
+    if (!isPasswordValid) throw new InvalidUserData();
+    req.session.user = User;
+    const userToReturn: any = User.toJSON();
+    delete userToReturn.password;
+    delete userToReturn.id;
+    res.json(userToReturn);
+  } catch (error) {
+    if (error instanceof AppError) {
+      console.log(error);
+      return res.status(error.code).json({ errors: [error.message], type: error.type });
+    }
+    res.status(500).json({ errors: error });
   }
 };
