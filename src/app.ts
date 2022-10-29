157ca209c52b1e6da022d20afc704d5f9b91bff5
Jesus Liang <jesusliang96@gmail.com>
2022-10-29 19:19:22 +0200

    added express session

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,12 @@ import helmet from 'helmet';
 import compression from 'compression';
 import morgan from 'morgan';
 import { routes } from './utils';
+import session from 'express-session';
+import config from './config';
+import ConfigueSequelizeStore from 'connect-session-sequelize';
+import { sequelize } from './database';
+
+const SequelizeStore = ConfigueSequelizeStore(session.Store);
 
 const app: Application = express();
 
@@ -13,6 +19,17 @@ app.use(compression());
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(cors());
+
+app.use(
+  session({
+    secret: config.SESSION_SECRET,
+    store: new SequelizeStore({
+      db: sequelize
+    }),
+    resave: false,
+    proxy: true
+  })
+);
 
 routes(app);
 
--- src/config.ts
+++ src/config.ts
@@ -12,7 +12,11 @@ const config = {
   DB_PORT: process.env.DB_PORT ?? 5432,
   DB_NAME: process.env.DB_name ?? 'hackaton',
   DB_USER: process.env.DB_USER ?? 'postgres',
-  DB_PASSWORD: process.env.DB_PASSWORD ?? 'postgres'
+  DB_PASSWORD: process.env.DB_PASSWORD ?? 'postgres',
+
+  // session config
+
+  SESSION_SECRET: process.env.SESSION_SECRET ?? 'Esto_es_de_alto_secreto_xd'
 };
 
 export default config;
