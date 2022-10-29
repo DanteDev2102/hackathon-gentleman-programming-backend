129eea0e46871feec24df9ec5a9782d692ff46b8
Jesus Liang <jesusliang96@gmail.com>
2022-10-29 17:53:24 +0200

    added user routes types

--- src/routes/user/user.routes.ts
+++ src/routes/user/user.routes.ts
@@ -1,12 +1,12 @@
-import { CreateUserSchema } from './../schemas/user/user.schema';
-import { IUser } from '@/database';
-import { validateSchema } from '../middlewares';
+import { CreateUserSchema } from '../../schemas/user/user.schema';
+import { validateSchema } from '../../middlewares';
 import { IRouter, Router } from 'express';
+import { CreateUserParams } from './user.routes.type';
 
 const userRoutes: IRouter = Router();
 
 userRoutes.post('/register', validateSchema(CreateUserSchema), async (req, res) => {
-  const user: IUser = req.body;
+  const user: CreateUserParams = req.body;
   console.log(user);
   res.send('user register');
 });
---
+++ src/routes/user/user.routes.type.ts
@@ -0,0 +1,7 @@
+export interface CreateUserParams {
+  email: string;
+  password: string;
+  passwordConfirm: string;
+  firstName: string;
+  lastName: string;
+}
