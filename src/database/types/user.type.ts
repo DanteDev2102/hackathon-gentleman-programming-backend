bb48608a150e90641b3096780e62055da5dfb560
Jesus Liang <jesusliang96@gmail.com>
2022-10-29 17:49:14 +0200

    created user routes

--- src/database/types/user.type.ts
+++ src/database/types/user.type.ts
@@ -1,6 +1,6 @@
 export interface IUser {
-  username: string;
-  password: string;
   email: string;
-  avatar?: string;
+  password: string;
+  firstName: string;
+  lastName: string;
 }
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,8 +1,13 @@
+import { CreateUserSchema } from './../schemas/user/user.schema';
+import { IUser } from '@/database';
+import { validateSchema } from '../middlewares';
 import { IRouter, Router } from 'express';
 
 const userRoutes: IRouter = Router();
 
-userRoutes.post('/register', async (_req, res) => {
+userRoutes.post('/register', validateSchema(CreateUserSchema), async (req, res) => {
+  const user: IUser = req.body;
+  console.log(user);
   res.send('user register');
 });
 
